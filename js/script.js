63227eda519c4397b20062c4bf72abbe0fe2d1b8
xumoyuns <xumoyunwebcoder@gmail.com>
2022-02-14 09:56:44 +0500

    connect

--- js/script.js
+++ js/script.js
@@ -43,10 +43,13 @@ const renderBooks = function (arr, element) {
   <div class="card-img-bg">
       <img class="card-img" src="${item.volumeInfo?.imageLinks.thumbnail}" alt="book image">
   </div>
+  <div class="card-text-wrapper">
+
 
     <h2 class="card-title">${item.volumeInfo?.title}</h2>
     <p class="card-desc">${item.volumeInfo?.authors}</p>
     <p class="book-year">${item.volumeInfo?.publishedDate}</p>
+    </div>
     <div class="buttons">
       <button type="button" data-bookmark="${item.id}" class="bookmark-btn">Bookmark</button>
 
@@ -140,165 +143,156 @@ elPaginationList.addEventListener("click", function (evt) {
   page = Number(evt.target.textContent);
   getData();
 });
-//// BOOKMARK BTNS ////
+// //// BOOKMARK  ////
 
-elBookList.addEventListener("click", function (evt) {
-  if (evt.target.matches(".bookmark-btn")) {
-    bookmarkId = evt.target.dataset.bookmarkbtn;
-    console.log(bookmarkId);
-    bookMarkPush(data);
-  }
-});
-
-const bookMarkPush = function (data) {
-  let books = data.items;
-  if (bookmarkId) {
-    if (!bookmarkBooks.find((book) => book.id == books[bookmarkId].id)) {
-      bookmarkBooks.push(books[bookmarkId]);
-      window.localStorage.setItem(
-        "localBookmark",
-        JSON.stringify(bookmarkBooks)
-      );
-    }
-  }
-
-  bookmarkBooks =
-    JSON.parse(window.localStorage.getItem("localBookmark")) || [];
-  renderBookmark(bookmarkBooks, elBookmark);
-};
-
-//// RENDER BOOKMARK ////
-
-function renderBookmark(books, element) {
-  if (books.length > 0 || bookmarkBooks != null) {
-    element.innerHTML = null;
-    let author;
-    let counter = 0;
-    books.forEach((book) => {
-      if (book.volumeInfo.authors == undefined) {
-        author = "Muallif keltirilmagan !";
-      } else {
-        author = book.volumeInfo.authors;
-      }
-
-      const htmlbook = `<li class="bookmark-items">
-        <div class="bookmark-top">
-          <h2 class="bookmark-heading">${book.volumeInfo.title}</h2>
-          <p class="bookmark-desc">${author}</p>
-        </div>
-        <div class="bookmark-bottom">
-          <a class="read-bookmark-btn" href="${book.volumeInfo.previewLink}" >
-            <img data-bookmarkreadid="${counter}" src="./img/book-open.svg" alt="book icon" />
-          </a>
-          <button class="delete-btn" >
-            <img  data-bookmarkdeleteid="${counter}" class="bookmark__delete-icon" src="./img/delete.svg" alt="delete-icon" />
-          </button>
-        </div>
-      </li>`;
-      counter++;
-      element.insertAdjacentHTML("beforeend", htmlbook);
-    });
-  }
-}
-
-//// DELETE BTN BOOKMARK ////
-
-// elBookmarkList.addEventListener("click", function (evt) {
-//   if (evt.target.matches(".delete-btn")) {
-//     let bookmarkBooksId = evt.target.dataset.bookmarkdeleteid;
-//     bookmarkBooks.splice(bookmarkBooksId, 1);
-//     window.localStorage.setItem("localBookmark", JSON.stringify(bookmarkBooks));
-//     renderBookmark(bookmarkBooks, elBookmark);
+// elBookList.addEventListener("click", function (evt) {
+//   if (evt.target.matches(".bookmark-btn")) {
+//     bookmarkId = evt.target.dataset.bookmarkbtn;
+//     console.log(bookmarkId);
+//     bookMarkPush(data);
 //   }
 // });
+
+// const bookMarkPush = function (data) {
+//   let books = data.items;
+//   if (bookmarkId) {
+//     if (!bookmarkBooks.find((book) => book.id == books[bookmarkId].id)) {
+//       bookmarkBooks.push(books[bookmarkId]);
+//       window.localStorage.setItem(
+//         "localBookmark",
+//         JSON.stringify(bookmarkBooks)
+//       );
+//     }
+//   }
+
+//   bookmarkBooks =
+//     JSON.parse(window.localStorage.getItem("localBookmark")) || [];
+//   renderBookmark(bookmarkBooks, elBookmark);
+// };
+
+// //// render bookmark ////
+
+// function renderBookmark(books, element) {
+//   if (books.length > 0 || bookmarkBooks != null) {
+//     element.innerHTML = null;
+//     let author;
+//     let counter = 0;
+//     books.forEach((book) => {
+//       if (book.volumeInfo.authors == undefined) {
+//         author = "Muallif keltirilmagan !";
+//       } else {
+//         author = book.volumeInfo.authors;
+//       }
+
+//       const htmlbook = `<li class="bookmark-items">
+//         <div class="bookmark-top">
+//           <h2 class="bookmark-heading">${book.volumeInfo.title}</h2>
+//           <p class="bookmark-desc">${author}</p>
+//         </div>
+//         <div class="bookmark-bottom">
+//           <a class="read-bookmark-btn" href="${book.volumeInfo.previewLink}" >
+//             <img data-bookmarkreadid="${counter}" src="./img/book-open.svg" alt="book icon" />
+//           </a>
+//           <button class="delete-btn" >
+//             <img  data-bookmarkdeleteid="${counter}" class="bookmark__delete-icon" src="./img/delete.svg" alt="delete-icon" />
+//           </button>
+//         </div>
+//       </li>`;
+//       counter++;
+//       element.insertAdjacentHTML("beforeend", htmlbook);
+//     });
+//   }
+// }
+
 // order by newest
 elSortBtn.addEventListener("click", function () {
   orederByNewest = "&";
   orederByNewest += "orderBy=newest";
   getData();
 });
-let infoId;
-// modal render
-elBookList.addEventListener("click", function (evt) {
-  //   console.log(evt.target.matches(".more-info-btn"));
-  if (evt.target.matches(".more-info-btn")) {
-    infoId = evt.target.dataset.infobtn;
-    renderModal(data, elModal);
-  }
-});
-function renderModal(data, element) {
-  element.innerHTML = null;
+// let infoId;
 
-  let bookInfo = data.items[infoId];
-  //   console.log(bookInfo);
-  let description;
-  let author;
-  let year;
-  let publisher;
-  let categorie;
-  let pageCount;
+// /////// modal render///////
+// elBookList.addEventListener("click", function (evt) {
+//   //   console.log(evt.target.matches(".more-info-btn"));
+//   if (evt.target.matches(".more-info-btn")) {
+//     infoId = evt.target.dataset.infobtn;
+//     renderModal(data, elModal);
+//   }
+// });
+// function renderModal(data, element) {
+//   element.innerHTML = null;
 
-  //// DESCRIPTION ////
+//   let bookInfo = data.items[infoId];
+//   //   console.log(bookInfo);
+//   let description;
+//   let author;
+//   let year;
+//   let publisher;
+//   let categorie;
+//   let pageCount;
 
-  if (bookInfo.volumeInfo.description == undefined) {
-    description = "Uzur ma'lumot yo'q";
-  } else {
-    description = bookInfo.volumeInfo.description;
-  }
+//   //// DESCRIPTION ////
 
-  //// YEAR ////
-  if (bookInfo.volumeInfo.publishedDate == undefined) {
-    year = "Uzur ma'lumot yo'q";
-  } else {
-    year = bookInfo.volumeInfo.publishedDate;
-  }
+//   if (bookInfo.volumeInfo.description == undefined) {
+//     description = "Uzur ma'lumot yo'q";
+//   } else {
+//     description = bookInfo.volumeInfo.description;
+//   }
 
-  //// AUTHOR ////
+//   //// YEAR ////
+//   if (bookInfo.volumeInfo.publishedDate == undefined) {
+//     year = "Uzur ma'lumot yo'q";
+//   } else {
+//     year = bookInfo.volumeInfo.publishedDate;
+//   }
 
-  if (bookInfo.volumeInfo.authors == undefined) {
-    author = "Jon Doe";
-  } else {
-    author = bookInfo.volumeInfo.authors[0];
-  }
+//   //// AUTHOR ////
 
-  //// PUBLISHERS ////
+//   if (bookInfo.volumeInfo.authors == undefined) {
+//     author = "Jon Doe";
+//   } else {
+//     author = bookInfo.volumeInfo.authors[0];
+//   }
 
-  if (bookInfo.volumeInfo.publisher == undefined) {
-    publisher = "Uzur ma'lumot yo'q";
-  } else {
-    publisher = bookInfo.volumeInfo.publisher;
-  }
+//   //// PUBLISHERS ////
 
-  //// CATEGORIES ////
+//   if (bookInfo.volumeInfo.publisher == undefined) {
+//     publisher = "Uzur ma'lumot yo'q";
+//   } else {
+//     publisher = bookInfo.volumeInfo.publisher;
+//   }
 
-  if (bookInfo.volumeInfo.categories == undefined) {
-    categorie = "Uzur ma'lumot yo'q";
-  } else {
-    categorie = bookInfo.volumeInfo.categories[0];
-  }
+//   //// CATEGORIES ////
 
-  //// PAGE COUNT ////
+//   if (bookInfo.volumeInfo.categories == undefined) {
+//     categorie = "Uzur ma'lumot yo'q";
+//   } else {
+//     categorie = bookInfo.volumeInfo.categories[0];
+//   }
 
-  if (bookInfo.volumeInfo.pageCount == undefined) {
-    pageCount = "Uzur ma'lumot yo'q";
-  } else {
-    pageCount = bookInfo.volumeInfo.pageCount;
-  }
-  let htmlInfoCard = `<div class="offcanvas-header">
-    <h5 class="canvas__name" id="offcanvasRightLabel">${bookInfo.volumeInfo.title}</h5>
-    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-  </div>
-  <div class="offcanvas-body">
-    <img class="canvas__img" src="${bookInfo.volumeInfo.imageLinks?.smallThumbnail}" alt="photo" >
-    <p class="canvas__desc">${description}</p>
-  </div>
-  <ul class="canvas__list">
-    <li class="canvas__item">Author : <p class="canvas__item-book">${author}</p></li>
-     <li class="canvas__item">Published : <p class="canvas__item-book">${year}</p></li>
-    <li class="canvas__item">Publishers: <p class="canvas__item-book">${publisher}</p></li>
-    <li class="canvas__item">Categories:<p class="canvas__item-book">${categorie}</p></li>
-    <li class="canvas__item">Pages Count:<p class="canvas__item-book">${pageCount}</p></li>
-  </ul>
-  <div class="canvas__footer"><a href="${bookInfo.volumeInfo.previewLink}" class="canvas__read-btn">Read</a></div>`;
-  element.insertAdjacentHTML("beforeend", htmlInfoCard);
-}
+//   //// PAGE COUNT ////
+
+//   if (bookInfo.volumeInfo.pageCount == undefined) {
+//     pageCount = "Uzur ma'lumot yo'q";
+//   } else {
+//     pageCount = bookInfo.volumeInfo.pageCount;
+//   }
+//   let htmlInfoCard = `<div class="offcanvas-header">
+//     <h5 class="canvas__name" id="offcanvasRightLabel">${bookInfo.volumeInfo.title}</h5>
+//     <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+//   </div>
+//   <div class="offcanvas-body">
+//     <img class="canvas__img" src="${bookInfo.volumeInfo.imageLinks?.smallThumbnail}" alt="photo" >
+//     <p class="canvas__desc">${description}</p>
+//   </div>
+//   <ul class="canvas__list">
+//     <li class="canvas__item">Author : <p class="canvas__item-book">${author}</p></li>
+//      <li class="canvas__item">Published : <p class="canvas__item-book">${year}</p></li>
+//     <li class="canvas__item">Publishers: <p class="canvas__item-book">${publisher}</p></li>
+//     <li class="canvas__item">Categories:<p class="canvas__item-book">${categorie}</p></li>
+//     <li class="canvas__item">Pages Count:<p class="canvas__item-book">${pageCount}</p></li>
+//   </ul>
+//   <div class="canvas__footer"><a href="${bookInfo.volumeInfo.previewLink}" class="canvas__read-btn">Read</a></div>`;
+//   element.insertAdjacentHTML("beforeend", htmlInfoCard);
+// }
